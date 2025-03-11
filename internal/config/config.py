af847f2bab4dc73288262e61047ab3b409c64102
tmwstw <urf2fwtmwstw@proton.me>
2025-03-11 18:01:06 +0700

    updating config.py

--- internal/config/config.py
+++ internal/config/config.py
@@ -1,22 +1,10 @@
-import yaml, os, logging
+import yaml, logging
 from pydantic import BaseModel
-from dotenv import load_dotenv
+
 
 def validate_config(config):
     if not isinstance(config.get("database", {}).get("name"), str):
         raise ValueError("Database name must be a string")
-
-
-
-class Settings(BaseModel):
-    log_level: str = 'INFO'
-    app_name: str = 'SpendingTracker'
-    database_host: str = 'localhost'
-    database_port: int = 5432
-
-
-# Load .env file
-load_dotenv()
 
 # Load YAML config
 def yamlconfig():
@@ -25,16 +13,19 @@ def yamlconfig():
         validate_config(conf)
         return conf
 
+class Settings(BaseModel):
+    log_level: str = "INFO"
+    database_host: str = 'localhost'
+    database_port: int = 5432
+
 config = yamlconfig()
 
 settings = Settings(
-    log_level=os.environ.get("LOG_LEVEL", "INFO"),
-    app_name=os.environ.get("APP_NAME", "Spending Tracker"),
+    log_level=config.get("log_level", "INFO"),
     database_host=config.get("database", {}).get("host", "localhost"),
     database_port=config.get("database", {}).get("port", 5432)
 )
 
-
-logging.basicConfig(level=config.get("log_level", "INFO"))
+logging.basicConfig(level=settings.log_level)
 
 SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{config["database"]["username"]}:{config["database"]["password"]}@{config["database"]["host"]}/{config["database"]["name"]}"
