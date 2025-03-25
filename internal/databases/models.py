9de01b519b5ce19cb5e81af9d6f92228ede11469
tmwstw <urf2fwtmwstw@proton.me>
2025-03-25 12:46:08 +0700

    feat: If applide, thcs commit will add new migration
    Make required lums in users table not nullable.
    Create new migration.

--- internal/databases/models.py
+++ internal/databases/models.py
@@ -20,8 +20,8 @@ class User(Base):
     __tablename__ = "users"
 
     user_id = Column(UUID(as_uuid=True), primary_key=True)
-    user_name = Column(String, unique=True)
-    hashed_password = Column(String)
+    username = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
 
 class Category(Base):
     __tablename__ = "categories"
