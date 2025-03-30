12cfdd18c0c3b83b6843dbcc45d416f9d5387bf0
tmwstw <urf2fwtmwstw@proton.me>
2025-03-30 15:08:07 +0700

    feat: Add user id filter for get all transactions/categories methods

--- internal/services/transaction_service.py
+++ internal/services/transaction_service.py
@@ -6,8 +6,8 @@ class TransactionService:
     def __init__(self, repo: TransactionsRepository):
         self.repo = repo
 
-    async def get_transactions(self, session):
-        transactions = await self.repo.get_transactions(session)
+    async def get_transactions(self, session, user_id):
+        transactions = await self.repo.get_transactions(session, user_id)
         return transactions
 
     async def add_transaction(self, session, new_transaction):
