bb897eadda320e424173068578ce868aa7a1b34f
tmwstw <urf2fwtmwstw@proton.me>
2025-04-04 12:51:50 +0700

    feat: Create conftest.py with fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from internal.application.routers import handlers
+from main import app
+
+
+
+@pytest.fixture(scope="session")
+def client():
+    handlers(app)
+    with TestClient(app) as client:
+        yield client
+
+@pytest.fixture(scope="module")
+def registered_test_user_data():
+    return {
+        "username": "test_username",
+        "password": "password123"
+    }
