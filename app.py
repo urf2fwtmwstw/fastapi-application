ea0600fba4e1c64655d7eaa24a49917901e0d26a
tmwstw <urf2fwtmwstw@proton.me>
2025-04-19 00:51:17 +0700

    fix: Update report generation job

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
-from asyncio import run
+import asyncio
 from contextlib import asynccontextmanager
+from datetime import UTC, datetime
 
 from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.triggers.cron import CronTrigger
 from fastapi import FastAPI
 
+from internal.auth.repository.users import UsersRepository
 from internal.databases.database import get_db
 from internal.reports.repository.reports_repository import ReportsRepository
 from internal.services.report_service import ReportService
 from internal.services.transaction_service import TransactionService
+from internal.services.user_service import UserService
 from internal.transactions.repository.transactions import TransactionsRepository
 
 resources = {}
 jobstores = {"default": MemoryJobStore()}
-scheduler = AsyncIOScheduler(jobstores=jobstores, timezone="Europe/Moscow")
-trigger = CronTrigger(year="*", month="*", day="18", hour="3", minute="16", second="0")
+scheduler = AsyncIOScheduler(jobstores=jobstores, timezone=UTC)
 
 
 @scheduler.scheduled_job("cron", day=1, hour=0, minute=0, second=0)
 async def generate_reports(
     service=ReportService(
-        ReportsRepository(), TransactionService(TransactionsRepository())
+        ReportsRepository(),
+        TransactionService(TransactionsRepository()),
+        UserService(UsersRepository()),
     ),
 ):
+    date = datetime.now(tz=UTC)
     async for db in get_db():
-        await service.add_report(db, "5f5a0067-e1f4-43ec-854d-c571850b61fd", 2025, 4)
+        asyncio.run(await service.async_report_generation(db, date.year, date.month))
 
 
 # OS signals handling
