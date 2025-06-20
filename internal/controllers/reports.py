91149776b2c48f6aacbd1c0649982f077f26e939
tmwstw <urf2fwtmwstw@proton.me>
2025-06-20 23:00:35 +0700

    fix: Remove report service initialization from lifespan
    Import resources from main app instead

--- internal/controllers/reports.py
+++ internal/controllers/reports.py
@@ -5,14 +5,14 @@ from http import HTTPStatus
 from typing import Annotated
 
 from aiokafka.errors import BrokerNotAvailableError, KafkaError
-from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException
+from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException, Request
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
-from app import app
+import app
 from internal.controllers.auth import get_auth_user_info
 from internal.databases.database import get_db
 from internal.logger.logger import logger
-from internal.reports.repository import ReportsRepository
+from internal.services.report_service import ReportService
 from internal.schemas.kafka_message_schema import KafkaFillReportMessage
 from internal.schemas.report_schema import (
     BlankReportSchema,
@@ -20,9 +20,6 @@ from internal.schemas.report_schema import (
     ReportSchema,
 )
 from internal.schemas.user_schema import UserSchema
-from internal.services.report_service import ReportService
-from internal.services.transaction_service import TransactionService
-from internal.transactions.repository import TransactionsRepository
 from internal.transport.consumer import Consumer
 from internal.transport.producer import KafkaProducerError, Producer
 
@@ -32,22 +29,12 @@ producer: Producer
 
 @asynccontextmanager
 async def lifespan(router: APIRouter):
-    global producer
-    # Initialize repositories and services
-    resources["report_repository"] = ReportsRepository()
-    resources["transaction_repository"] = TransactionsRepository()
-    resources["transaction_service"] = TransactionService(
-        resources["transaction_repository"]
-    )
-    resources["report_service"] = ReportService(
-        resources["report_repository"], resources["transaction_service"]
-    )
-
     # Initialize Kafka producer and consumer
+    global producer
     try:
         producer = Producer()
         await producer.report_producer.start()
-        kafka_consumer = Consumer(resources["report_service"])
+        kafka_consumer = Consumer(app.resources["services"]["report_service"])
         await kafka_consumer.report_consumer.start()
         asyncio.create_task(kafka_consumer.consume_create_report_message())
     except KafkaError as e:
@@ -62,22 +49,15 @@ async def lifespan(router: APIRouter):
 router = APIRouter(lifespan=lifespan)
 
 
-def get_transaction_service():
-    transaction_service = resources.get("transaction_service", None)
-    if transaction_service is None:
-        raise ModuleNotFoundError('"transaction_service" was not initialized')
-    return transaction_service
-
-
-def get_report_service():
-    report_service = resources.get("report_service", None)
+def get_report_service() -> ReportService:
+    report_service = app.resources["services"].get("report_service", None)
     if report_service is None:
         raise ModuleNotFoundError('"report_service" was not initialized')
     return report_service
 
 
-@app.exception_handler(KafkaProducerError)
-async def kafka_consumer_exception_handler(request, e: KafkaProducerError):
+@app.app.exception_handler(KafkaProducerError)
+async def kafka_consumer_exception_handler(request: Request, e: KafkaProducerError):
     logger.error(f"HTTP exception: {e.message}")
     return HTTPException(
         status_code=500,
