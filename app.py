578006b0075efd931d45431a681057ef3b7e52c2
tmwstw <urf2fwtmwstw@proton.me>
2025-06-25 00:10:07 +0700

    fix: Create scheduler package

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from contextlib import asynccontextmanager
-from datetime import UTC
 
-from apscheduler.jobstores.memory import MemoryJobStore
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from fastapi import FastAPI
+from internal.scheduler.scheduled_jobs import create_scheduled_jobs, scheduler
 
 from dependencies import get_dependency, initialize_dependencies
-from scheduled_jobs import create_scheduled_jobs
 
 resources = {}
 
@@ -16,11 +13,8 @@ resources = {}
 async def lifespan(app: FastAPI):
     print("Initializing resources...")
     await initialize_dependencies(resources)
-    jobstores = {"default": MemoryJobStore()}
-    scheduler = AsyncIOScheduler(jobstores=jobstores, timezone=UTC)
-    await create_scheduled_jobs(scheduler, await get_dependency("report_service", resources))
+    await create_scheduled_jobs(await get_dependency("report_service", resources))
     resources["message"] = "Initialized"
-    scheduler.start()
     yield
     print("Cleaning up resources...")
     resources.clear()
--- internal/scheduler/scheduled_jobs.py
+++ internal/scheduler/scheduled_jobs.py
@@ -1,14 +1,16 @@
 from datetime import UTC, datetime
 
+from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from internal.databases.database import get_db
 from internal.services.report_service import ReportService
 
+jobstores = {"default": MemoryJobStore()}
+scheduler = AsyncIOScheduler(jobstores=jobstores, timezone=UTC)
 
-async def create_scheduled_jobs(
-    scheduler: AsyncIOScheduler, report_service: ReportService
-) -> None:
+
+async def create_scheduled_jobs(report_service: ReportService) -> None:
     @scheduler.scheduled_job("cron", day=20, hour=14, minute=26, second=0)
     async def create_reports(service=report_service):
         date = datetime.now(tz=UTC)
@@ -25,3 +27,5 @@ async def create_scheduled_jobs(
     async def fill_created_reports(service=report_service):
         async for db in get_db():
             await service.fill_created_reports(db)
+
+    scheduler.start()
